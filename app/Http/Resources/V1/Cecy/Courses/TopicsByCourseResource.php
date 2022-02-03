35c1c8885a071901cf66e92509d35d6e79b89840
khabLexander <mantillagka@hotmail.com>
2022-02-03 15:55:00 -0500

    gg

--- app/Http/Resources/V1/Cecy/Courses/TopicsByCourseResource.php
+++ app/Http/Resources/V1/Cecy/Courses/TopicsByCourseResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\V1\Cecy\Courses;
 
+use App\Http\Resources\V1\Cecy\Topics\TopicResource;
 use App\Models\Cecy\Catalogue;
 use App\Models\Cecy\Course;
 use App\Models\Cecy\Topic;
@@ -13,9 +14,9 @@ class TopicsByCourseResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'course.id' => Course::make($this->course),
-            'level.id' => Catalogue::make($this->level_id),
-            'parent.id' => Topic::make($this->parent_id),
+            'course.id' => CourseResource::make($this->course),
+            'level' => $this->level,
+            'parent.id' => TopicResource::make($this->children),
             'description' => $this->description
         ];
     }
