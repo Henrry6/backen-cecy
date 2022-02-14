ae1d093da5f96a7411e15c048ed74b80b58e11d6
J3niff3r Guañuna <jeny.guanuna@gmail.com>
2022-02-14 02:01:05 -0500

    'change'

--- routes/api/v1/private/cecy.php
+++ routes/api/v1/private/cecy.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\V1\Cecy\CatalogueController;
 use App\Http\Controllers\V1\Cecy\CertificateController;
 use App\Http\Controllers\V1\Cecy\ClassroomController;
 use App\Http\Controllers\V1\Cecy\GuachagmiraController;
-use App\Http\Controllers\V1\Cecy\GuanunaController;
 use App\Http\Controllers\V1\Cecy\PerezController;
 use App\Http\Controllers\V1\Cecy\CourseController;
 use App\Http\Controllers\V1\Cecy\DetailAttendanceController;
