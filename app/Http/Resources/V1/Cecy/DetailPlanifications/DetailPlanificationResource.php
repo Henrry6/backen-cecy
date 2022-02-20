868b4c376dc421f8ac8acbf27297318ee7cbb3ca
1725273823 <kam.guachagmira@yavirac.edu.ec>
2022-02-20 15:09:11 -0500

    bfore fix intrscutprs seed

--- app/Http/Resources/V1/Cecy/DetailPlanifications/DetailPlanificationResource.php
+++ app/Http/Resources/V1/Cecy/DetailPlanifications/DetailPlanificationResource.php
@@ -5,9 +5,11 @@ namespace App\Http\Resources\V1\Cecy\DetailPlanifications;
 use App\Http\Resources\V1\Cecy\Catalogues\CatalogueResource;
 use App\Http\Resources\V1\Cecy\Classrooms\ClassroomResource;
 use App\Http\Resources\V1\Cecy\Courses\CourseResource;
+use App\Http\Resources\V1\Cecy\Instructors\InstructorResource;
 use App\Http\Resources\V1\Cecy\Planifications\PlanificationResource;
 use App\Http\Resources\V1\Cecy\Registrations\RegistrationResource;
 use App\Models\Cecy\Classroom;
+use App\Models\Cecy\Instructor;
 use App\Models\Cecy\Registration;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -24,6 +26,7 @@ class DetailPlanificationResource extends JsonResource
             'workday' => CatalogueResource::make($this->workday),
             'state' => CatalogueResource::make($this->state),
             'registrations' => RegistrationResource::collection($this->registrations),
+            'instructors' => InstructorResource::collection($this->instructors),
             'endedTime' => $this->ended_time,
             'observations' => $this->observations,
             'planEndedAt' => $this->plan_ended_at,
