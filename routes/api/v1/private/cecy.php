a6dfdc0914ef2522dc27f88057d3fd2b1ba87510
1727326520 <ferchofercho933@gmail.com>
2022-02-23 19:41:27 -0500

    changes

--- routes/api/v1/private/cecy.php
+++ routes/api/v1/private/cecy.php
@@ -128,7 +128,7 @@ Route::prefix('courses')->group(function () {
 
 Route::prefix('courses')->group(function () {
     Route::get('', [CourseController::class, 'getCourses']);
-    Route::get('inform-course-needs/{course}', [CourseController::class, 'informCourseNeeds']);
+    // Route::get('inform-course-needs/{course}', [CourseController::class, 'informCourseNeeds']);
 
 });
 
@@ -156,7 +156,7 @@ Route::prefix('courses/{course}')->group(function () {
     Route::patch('assign-code', [CourseController::class, 'assignCodeToCourse']);
     Route::patch('not-approve-reason', [CourseController::class, 'notApproveCourseReason']);
     // Route::get('inform-course-needs/{course}', 'App\Http\Controllers\V1\Cecy\CourseController@informCourseNeeds');
-    // Route::get('inform-course-needs/{course}', [CourseController::class, 'informCourseNeeds']);
+     Route::get('inform-course-needs', [CourseController::class, 'informCourseNeeds']);
     Route::get('curricular-design', [CourseController::class, 'showCurricularDesign']);
     Route::get('final-report', [CourseController::class, 'showCourseFinalReport']);
 });
