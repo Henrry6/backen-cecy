9e2efadb4bfe35ce26f523f129ff50fb37851d1a
1725273823 <kam.guachagmira@yavirac.edu.ec>
2022-03-02 19:02:29 -0500

    before checkout

--- routes/api/v1/public/cecy.php
+++ routes/api/v1/public/cecy.php
@@ -12,11 +12,24 @@ Route::prefix('cecy-catalogue')->group(function () {
     Route::get('catalogue', [CatalogueController::class, 'catalogue']);
 });
 
+/***********************************************************************************************************************
+ * USERS/PARTICIPANT
+ **********************************************************************************************************************/
 Route::prefix('participant-user')->group(function () {
     Route::post('registration', [ParticipantController::class, 'registerParticipantUser']);
 });
 
+/***********************************************************************************************************************
+ * COURSES
+ **********************************************************************************************************************/
 Route::prefix('courses-guest')->group(function () {
     Route::get('public-courses', [CourseController::class, 'getPublicCourses']);
     Route::get('public-courses-category/{category}', [CourseController::class, 'getPublicCoursesByCategory']);
+});
+
+Route::prefix('courses-guest/{course}')->group(function () {
+    Route::prefix('image')->group(function () {
+        Route::get('{image}', [CourseController::class, 'showImageCourse']);
+        Route::post('', [CourseController::class, 'uploadImageCourse']);
+    });
 });
