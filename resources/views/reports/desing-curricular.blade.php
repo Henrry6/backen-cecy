18fd2ad4e39d261ca6acec09be0ed0686685b846
sebastiansalazar1997 <isb.salazar@yavirac.edu.ec>
2022-03-08 13:42:35 -0500

    formato

---
+++ resources/views/reports/desing-curricular.blade.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Diseño Curricular</title>
+</head>
+
+<body>
+  <div class="col-20">
+    <div>
+      <b>FORMULARIO: b1</b>
+      <h3 style="text-align:center ">DIRECCIÓN DE CALIFICACIÓN Y RECONOCIMIENTO <br>FORMULARIO DE DISEÑO CURRICULAR<br>
+        - CAPACITACIÓN CONTINUA -</h3>
+      <h4>Identificación del Curso.</h4>
+
+
+    </div>
+    <br><br>
+
+    <div class="table_1 ">
+
+      <table align="">
+
+
+        <tr>
+          <th class="column">Nombre del curso</th>
+          <th>ÁREA</th>
+          <th></th>
+          <th>ESPEC.</th>
+          <th></th>
+          <th>ALINEACIONAL EJE DE LA ANC</th>
+
+        </tr>
+        <tr>
+          <td>FORMADOR DE FORMADORES</td>
+          <td>A</td>
+          <td>ADMINISTRACION Y LEGISLACION </td>
+          <td>A.4</td>
+          <td>Evaluacion de proyectos <br> (Economia financiera)</td>
+          <td></td>
+
+        </tr>
+      </table>
+    </div>
+
+
+    <br> <br>
+
+    <div class="table_2 ">
+
+      <table align="">
+
+
+        <tr>
+          <th class="column">Tipo de participante</th>
+          <th></th>
+          <th>Modalidad</th>
+          <th></th>
+          <th>Duracion</th>
+
+        </tr>
+        <tr>
+          <td>Docentes, facilitadores, capacitadores y <br> expertos en diferentes áreas técnicas, <br> tecnológicas y de especialización</td>
+          <td></td>
+          <td>Virtual</td>
+          <td></td>
+          <td>40 horas </td>
+
+        </tr>
+      </table>
+    </div>
+
+
+    <div class="col-11">
+      <h4>Requisitos minimos de entrada al curso</h4>
+      <p style="text-align:center ">Técnicos: Computadora, tablet o smartphone con acceso a internet / Navegadores instalados Mozilla Firefox, Google Chrome, Brave, Opera u otro en última versión / Zoom instalado / Microsoft
+        Office (Word, Excel, Power Point) / Adobe Acrobat Reader u otro lector PDF instalado y actualizado
+        Office (Word, Excel, Power Point) / Adobe Acrobat Reader u otro lector PDF instalado y actualizado </p>
+      <br>
+      <h4>Obejtivo del curso</h4>
+      <br>
+      <p style="text-align:center ">Desarrollar las competencias necesarias como Formador de Formadores, facilitando enfoques teóricos, metodologías y técnicas de enseñanza–aprendizaje requeridas para procesos exitosos en los ámbitos educativo y empresarial</p>
+      <br>
+      <h4>Contenido del curso</h4>
+      <div>Temas principales</div>
+    </div>
+
+
+
+    <div class="col-11 ">
+
+      <table align="">
+
+        <tr>
+          <td>1. El aprendizaje y la formación en adultos </td>
+
+        </tr>
+        <tr>
+          <td>2. El contexto de la formación profesional</td>
+
+        </tr>
+        <tr>
+          <td> 3. El proceso de formación profesional </td>
+
+        </tr>
+      </table>
+    </div>
+    <br><br>
+
+    <div class="col-11 ">
+      <p>Temas secundarios o sub temas </p>
+      <br>
+      <p>1.1. Educación, formación y pedagogía 1.2. Didáctica y aprendizaje integral 1.3. Andragogía 1.4. Estilos de aprendizaje 1.5. Diagnóstico motivacional 1.6. Género e inclusión </p>
+
+      <p>2.1. Contexto de la formación profesional 2.2. Marco legal del sistema de formaciòn profesional 2.3. Necesidades de formación profesional en la empresa </p>
+
+      <p>3.1. Planificación de la formación 3.2. Organización de la formación 3.3. Desarrollo de la formación 3.4. Evaluación de la formación 3.5. Cierre y retroalimenatción de la formación </p>
+    </div>
+    <br><br>
+
+    <div class="col-11 ">
+      <div>Temas transversales </div>
+      <br>
+
+      <table align="">
+
+        <tr>
+          <td>Modalidades de Formación Técnica y Tecnológica. Industria 4.0: El futuro del trabajo. Competencias digitales </td>
+
+        </tr>
+        <tr>
+          <td>Estrategias personales de estudio. Técnicas. Hábitos </td>
+
+        </tr>
+
+      </table>
+    </div>
+    <br>
+    <div class="col-11 ">
+      <h4>Estrategias de enseñanza - aprendizaje</h4>
+      <br>
+      <table align="">
+
+        <tr>
+          <td>Aprendizaje basado en Problemas, Aprendizaje orientado a Proyectos, Aprendizaje Coooperativo </td>
+
+        </tr>
+      </table>
+    </div>
+    <br>
+    <div class="table_3 ">
+      <h4>Mecanismos de evalucion</h4>
+      <br>
+
+      <table align="">
+        <tr>
+          <th colspan="2">Evaluación diagnóstica</th>
+          <th colspan="2">Evaluación proceso formativo</th>
+          <th colspan="2">Evaluación final</th>
+
+
+        </tr>
+
+        <tr>
+          <th class="column">Tecnica</th>
+          <th>Instrumento</th>
+          <th>Tecnica</th>
+          <th>Instrumento</th>
+          <th>Tecnica</th>
+          <th>Instrumento</th>
+
+
+        </tr>
+        <tr>
+          <td>Encuesta</td>
+          <td>Cuestionario escrito</td>
+          <td>Talleres</td>
+          <td>Entregable</td>
+          <td>Proyecto</td>
+          <td>Proeycto escrito-entragble final</td>
+
+
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td>Tareas</td>
+          <td>Entregable</td>
+          <td>Examen</td>
+          <td>Cuestionario escrito</td>
+
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td>Prueba</td>
+          <td>Cuestionario escrito</td>
+          <td></td>
+          <td></td>
+
+        </tr>
+      </table>
+    </div>
+    <br>
+    <p>En caso de requerir más espacios, insertar filas.<br> * En cursos mayores a 40 horas</p>
+    <br><br>
+    <div class="table_4">
+      <table align="">
+
+        <tr>
+          <th colspan="3">Entorno de Aprendizaje (Equipos, maquinarias, herramientas, materiales, materiales didácticos y de consumo para el desarrollo de la oferta de capacitación).</th>
+        </tr>
+
+        <tr>
+          <th>Instalaciones</th>
+          <th>Fase teórica</th>
+          <th>Fase práctica</th>
+
+
+        </tr>
+        <tr>
+          <td>Aula virtual</td>
+          <td>x</td>
+          <td></td>
+
+
+
+        </tr>
+        <tr>
+          <td>Plataforma Google Suite Yavirac</td>
+          <td></td>
+          <td>x</td>
+
+        </tr>
+        <tr>
+          <td colspan="3"></td>
+        </tr>
+
+        <tr>
+          <td rowspan="2">Carga horaria</td>
+          <td>Horas practicas</td>
+          <td>20 horas</td>
+        </tr>
+
+
+        <tr>
+          <td>Horas teóricas</td>
+          <td>20 horas</td>
+        </tr>
+
+        <tr>
+          <td>Bibliografia</td>
+          <td colspan="2">AQUI VA UNA BIBLIOGRAFIA</td>
+        </tr>
+      </table>
+
+    </div>
+
+    <br><br><br><br><br><br>
+    <h4>.........................................................................<br>
+      Firma del responsable del diseño curricular</h4>
+    <br>
+    <div1>Nombre y <br> apellido: </div1>
+    <div2>LUIS PATRICIO VICENTE <br> GUTIEERRES</div2>
+    <br><br>
+    <div>Cedula</div>
+    <div>17xxxxxxx</div>
+
+    <div class="col-11">
+      <table align="center">
+
+        <tr>
+          <td>Nota: En el caso de disponer sólo en archivos digitales, se puede insertar las firmas de resposabilidad como imagen en las celdas correspondientes.
+          </td>
+
+        </tr>
+      </table>
+    </div>
+
+  </div>
+  <style>
+    table,
+    th,
+    td {
+      border: 1px solid black;
+      border-collapse: collapse;
+      width: 80%;
+      height: 30px;
+
+    }
+
+    th,
+    td {
+      padding: 10px;
+      text-align: center;
+      height: auto;
+      width: auto;
+
+    }
+
+    th {
+      background-color: #C0FFF4;
+    }
+
+
+
+    p {
+      white-space: pre-line;
+      font-size: 1em;
+
+    }
+
+    .col-20 {
+      font-family: 'calibri';
+      position: relative;
+
+
+    }
+
+    b {
+      color: red;
+    }
+
+    body {
+      margin-left: 50px;
+      margin-right: 50px;
+      margin-top: 500px;
+      margin-bottom: 10px;
+    }
+
+    div1 {
+      float: left;
+      height: 100%
+    }
+
+    div2 {
+      float: left;
+      height: 100%;
+      margin-left: 70px;
+    }
+  </style>
+</body>
+
+</html>
