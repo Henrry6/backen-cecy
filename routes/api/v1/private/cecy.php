60255c9093d02c28dd95e59b9f53a08321b1e19a
1754407938 <sav.mina@yavirac.edu.ec>
2022-04-05 15:24:43 -0500

    cecy

--- routes/api/v1/private/cecy.php
+++ routes/api/v1/private/cecy.php
@@ -25,28 +25,34 @@ use App\Http\Controllers\V1\Cecy\ParticipantController;
 /***********************************************************************************************************************
  * CATALOGUES
  **********************************************************************************************************************/
-Route::prefix('catalogue/{catalogue}')->group(function () {
-    Route::prefix('file')->group(function () {
-        Route::get('{file}/download', [CatalogueController::class, 'downloadFile']);
-        Route::get('download', [CatalogueController::class, 'downloadFiles']);
-        Route::get('', [CatalogueController::class, 'indexFiles']);
-        Route::get('{file}', [CatalogueController::class, 'showFile']);
-        Route::post('', [CatalogueController::class, 'uploadFile']);
-        Route::post('{file}', [CatalogueController::class, 'updateFile']);
-        Route::delete('{file}', [CatalogueController::class, 'destroyFile']);
-        Route::patch('', [CatalogueController::class, 'destroyFiles']);
+
+///catalogue/catalogue 
+Route::controller(InstitutionController::class)->group(function () {
+    Route::prefix('catalogue/{catalogue}')->group(function () {
+        Route::prefix('file')->group(function () {
+            Route::get('{file}/download', [CatalogueController::class, 'downloadFile']);
+            Route::get('download', [CatalogueController::class, 'downloadFiles']);
+            Route::get('', [CatalogueController::class, 'indexFiles']);
+            Route::get('{file}', [CatalogueController::class, 'showFile']);
+            Route::post('', [CatalogueController::class, 'uploadFile']);
+            Route::post('{file}', [CatalogueController::class, 'updateFile']);
+            Route::delete('{file}', [CatalogueController::class, 'destroyFile']);
+            Route::patch('', [CatalogueController::class, 'destroyFiles']);
+        });
+        Route::prefix('image')->group(function () {
+            Route::get('{image}/download', [CatalogueController::class, 'downloadImage'])->withoutMiddleware('auth:sanctum');
+            Route::get('', [CatalogueController::class, 'indexImages']);
+            Route::get('{image}', [CatalogueController::class, 'showImage']);
+            Route::post('', [CatalogueController::class, 'uploadImage']);
+            Route::post('{image}', [CatalogueController::class, 'updateImage']);
+            Route::delete('{image}', [CatalogueController::class, 'destroyImage']);
+            Route::patch('', [CatalogueController::class, 'destroyImages']);
+        });
     });
-    Route::prefix('image')->group(function () {
-        Route::get('{image}/download', [CatalogueController::class, 'downloadImage'])->withoutMiddleware('auth:sanctum');
-        Route::get('', [CatalogueController::class, 'indexImages']);
-        Route::get('{image}', [CatalogueController::class, 'showImage']);
-        Route::post('', [CatalogueController::class, 'uploadImage']);
-        Route::post('{image}', [CatalogueController::class, 'updateImage']);
-        Route::delete('{image}', [CatalogueController::class, 'destroyImage']);
-        Route::patch('', [CatalogueController::class, 'destroyImages']);
+    Route::prefix('catalogue')->group(function () {
+        Route::get('catalogue', [CatalogueController::class, '']);
     });
 });
-
 /***********************************************************************************************************************
  * INSTITUTIONS
  **********************************************************************************************************************/
