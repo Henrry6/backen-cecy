d28414d20abf2f42328f98f127afb9641ed4b475
Fernando Rivera <ferchofercho933@gmail.com>
2022-07-01 12:36:28 -0500

    changes poa

--- app/Http/Controllers/V1/Cecy/PlanificationController.php
+++ app/Http/Controllers/V1/Cecy/PlanificationController.php
@@ -461,7 +461,7 @@ class PlanificationController extends Controller
 
     public function storeAnnualOperativePlan(StoreAnnualOperativePlanRequest $request)
     {
-        // DDRC-C: crea una planificacion como parte de un plan anual
+        // DDRC-C: crea una planificacion como parte de un plan operativo anual
         $catalogue = json_decode(file_get_contents(storage_path() . "/catalogue.json"), true);
 
         $position = Catalogue::where('type', $catalogue['position']['type'])
