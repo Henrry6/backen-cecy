4d8841e6c2ed4949c30235da412ca5cbc683ee6e
AnthonySantillan <fairy.jum568@gmail.com>
2022-08-02 22:19:46 -0500

    photografic records

--- app/Http/Controllers/V1/Cecy/PhotographicRecordController.php
+++ app/Http/Controllers/V1/Cecy/PhotographicRecordController.php
@@ -129,8 +129,8 @@ class PhotographicRecordController extends Controller
     }
     //revisar
     public function getPhotograficRecord(DetailPlanification $detailPlanification){
-        $photographicRecords = $detailPlanification->photographicRecords()
-        ->where('registered_at' ,'=',now())->get();
+        $photographicRecords = $detailPlanification->photographicRecords()->get();
+//        ->where('registered_at' ,'=',now())->get();
         return(new PhotographicRecordCollection($photographicRecords))
             ->additional([
                 'msg' => [
